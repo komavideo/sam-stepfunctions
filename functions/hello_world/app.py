1d440a221dd7547b3e71911a895bd910392227e9
koma <koma@komavideo.com>
2022-12-20 23:57:59 +0900

    增加Lambda Layer

--- functions/hello_world/app.py
+++ functions/hello_world/app.py
@@ -1,8 +1,10 @@
 import random
+import myfunc as myfunc
 
 def lambda_handler(event, context):
     todoList = ["游泳", "爬山", "吃冰淇淋"]
     return {
         'statusCode': 200,
         'body': random.choice(todoList),
+        'result': myfunc.add(100, 200),
     }
